bc093b0992b0994a2a3ad1b8e9c4be09a31f51e5
ryancondones <130498054+ryancondones@users.noreply.github.com>
2024-08-14 13:29:56 -0400

    second commit

---
+++ attendance.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+import os
+import csv
+import time
+import pickle
+
+from sklearn.neighbors import KNeighborsClassifier
+from datetime import datetime
+
+video = cv2.VideoCapture(0)
+face_detection = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+with open('data/names.pkl', 'rb') as w:
+    LABELS = pickle.load(w)
+
+with open('data/face_data.pkl', 'rb') as f:
+    FACES = pickle.load(f)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(FACES, LABELS)
+
+img_bg = cv2.imread("bg.png")
+
+COL_NAMES = ['NAME', 'TIME']
+
+while True:
+    ret, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detection.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        crop_img = frame[y:y+h, x:x+w, :]
+        resized_img = cv2.resize(crop_img, (50, 50)).flatten().reshape(1, -1)
+        output = knn.predict(resized_img)
+        ts = time.time()
+        date = datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+        timestamp = datetime.fromtimestamp(ts).strftime("%H:%M-%S")
+        exist = os.path.isfile("attendance/attendance_" + date + ".csv")
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (50, 50, 255), 2)
+        cv2.rectangle(frame, (x, y-40), (x+w, y), (50, 50, 255), -1)
+        cv2.putText(frame, (x, y), (x+w, y+h), (50, 50, 255), 1)
+        attendance = [str(output[0]), str(timestamp)]
+    img_bg[162:162+480, 55:55+640] = frame
